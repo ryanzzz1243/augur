c7adbb8758c5cafe342367a62d5e7dc92db06ed5
jafk5w <jafk5w@mail.missouri.edu>
2022-12-17 04:32:45 -0600

    Implement tests for new metrics

---
+++ tests/test_routes/test_api_functionality/test_iota_routes_api.py
@@ -0,0 +1,10 @@
+#SPDX-License-Identifier: MIT
+import requests
+import pytest
+from tests import server_port
+
+
+def test_get_net_contributions_api_is_functional():
+    response = requests.get(f'http://localhost:{server_port}/api/unstable/iota/get_net_contributions')
+    data = response.json()
+    assert response.status_code == 200
